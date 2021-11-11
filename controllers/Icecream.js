bdbc43678a6d069530f7b9a7099dce37fba8e27a
VagulapuramSatish <satish.vagulapuram@gmail.com>
2021-11-10 23:33:53 -0600

    changed icecream file in views

--- controllers/Icecream.js
+++ controllers/Icecream.js
@@ -15,9 +15,25 @@ exports.Icecream_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: Icecream detail: ' + req.params.id);
 };
 // Handle Icecream create on POST.
-exports.Icecream_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: Icecream create POST');
-};
+exports.Icecream_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Icecream();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"Icecream_type":"goat", "cost":12, "size":"large"}
+    document.brand = req.body.brand;
+    document.flavour = req.body.flavour;
+    document.cost = req.body.cost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
 // Handle Icecream delete form on DELETE.
 exports.Icecream_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: Icecream delete DELETE ' + req.params.id);
