5627f313ebac9c48458ed166326908cfed00cdbb
VagulapuramSatish <satish.vagulapuram@gmail.com>
2021-11-11 21:38:04 -0600

    changed Icecream file in controllers

--- controllers/Icecream.js
+++ controllers/Icecream.js
@@ -28,7 +28,7 @@ exports.Icecream_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"Icecream_type":"goat", "cost":12, "size":"large"}
+    // {"Icecream_type":"goat", "flavour":12, "size":"large"}
     document.brand = req.body.brand;
     document.flavour = req.body.flavour;
     document.cost = req.body.cost;
@@ -46,9 +46,25 @@ exports.Icecream_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: Icecream delete DELETE ' + req.params.id);
 };
 // Handle Icecream update form on PUT.
-exports.Icecream_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: Icecream update PUT' + req.params.id);
-};
+exports.Icecream_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+   ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Icecream.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.Icecream_type)
+    toUpdate.brand = req.body.brand;
+    if(req.body.flavour) toUpdate.flavour = req.body.flavour;
+    if(req.body.cost) toUpdate.cost = req.body.cost;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+   failed`);
+    }
+   };
 // VIEWS
 // Handle a show all view
 exports.Icecream_view_all_Page = async function(req, res) {
