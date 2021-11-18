9f1214e8c27320c0cfda4985e53f44ac09e26864
VagulapuramSatish <satish.vagulapuram@gmail.com>
2021-11-17 18:27:38 -0600

    added Icecreamdelete file

--- controllers/Icecream.js
+++ controllers/Icecream.js
@@ -124,3 +124,16 @@ exports.Icecream_update_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
    };
+// Handle a delete one view with id from query
+exports.Icecream_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Icecream.findById(req.query.id)
+    res.render('Icecreamdelete', { title: 'Icecream Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/Icecream.js
+++ routes/Icecream.js
@@ -9,4 +9,6 @@ router.get('/detail', Icecream_controlers.Icecream_view_one_Page);
 router.get('/create', Icecream_controlers.Icecream_create_Page);
 /* GET create update page */
 router.get('/update', Icecream_controlers.Icecream_update_Page);
+/* GET create Icecream page */
+router.get('/delete', Icecream_controlers.Icecream_delete_Page);
 module.exports = router;
