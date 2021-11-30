facf665f9a8b9026a5128e04b8508ea160e50eb6
VagulapuramSatish <satish.vagulapuram@gmail.com>
2021-11-30 16:40:27 -0600

    added buttons in Icecream.pug

--- models/Icecream.js
+++ models/Icecream.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose")
-const IcecreamSchema = mongoose.Schema({brand: String,flavour: String,cost: Number})
+const IcecreamSchema = mongoose.Schema({brand: String,flavour: String,cost: {type:Number,min:15,max:100}})
 module.exports = mongoose.model("Icecream",IcecreamSchema)
